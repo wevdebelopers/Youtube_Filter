26eba7473fbe5cdf3c075eb94ea58b71882829c3
saeidanwar <mdsaeid753@gmail.com>
2023-07-24 21:03:00 +0530

    google verification implemented using OAuth

---
+++ Frontend/OAuth.js
@@ -0,0 +1,61 @@
+const OAuth = document.querySelector('.header__account_photo');
+OAuth.addEventListener('click', function() {
+    let oauth2ep = "https://accounts.google.com/o/oauth2/v2/auth";
+    let form = document.createElement("form");
+    form.setAttribute("method", "GET");
+    form.setAttribute("action", oauth2ep);
+    let params = {
+        "client_id" : "182985029199-aq3p34sjqeteo762eahvlllbpffjegns.apps.googleusercontent.com",
+        "redirect_uri" : "http://127.0.0.1:5500/Frontend/index.html",
+        "response_type" : "token",
+        "scope" : "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
+        "include_granted_scopes" : "true",
+        "state" : "pass-through-value"
+    }
+    for(var p in params)
+    {
+        let input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", p);
+        input.setAttribute("value", params[p]);
+        form.appendChild(input);
+    }
+    document.body.appendChild(form);
+    form.submit();
+    stats = 1;
+    console.log('OAuth was clicked!');
+});
+
+// var url = window.location;
+// var access_token = new URLSearchParams(window.location.hash).get('access_token');
+
+let params = {};
+let regex = /([^&=]+)=([^&]*)/g, m;
+let authInfo;
+while(m = regex.exec(window.location.hash))
+{
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+if(Object.keys(params).length > 0)
+{
+    localStorage.setItem("authInfo", JSON.stringify(params));
+}
+
+//hiding the access token
+window.history.pushState({}, document.title,"/Frontend/" + "index.html");
+
+//storing data
+let info = JSON.parse(localStorage.getItem("authInfo"));
+console.log(info);
+
+//calling API
+fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
+  headers:{
+    "Authorization":`Bearer ${info['access_token']}`
+  }
+})
+.then((data) => data.json())
+.then((info) => {
+  console.log(info);
+  document.getElementById('image').setAttribute('src', info.picture);
+})
