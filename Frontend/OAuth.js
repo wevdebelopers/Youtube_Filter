bbbebc106a1cc617b511bc8ecdb11ab2e7acc7b8
saeidanwar <mdsaeid753@gmail.com>
2023-07-27 15:13:33 +0530

    now all channels are showing

--- Frontend/OAuth.js
+++ Frontend/OAuth.js
@@ -60,47 +60,111 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
   document.getElementById('image').setAttribute('src', info.picture);
 })
 
-let channelList = [];
-channelList[0] = document.querySelector("#ch1");
-channelList[1] = document.querySelector("#ch2");
-channelList[2] = document.querySelector("#ch3");
-channelList[3] = document.querySelector("#ch4");
-channelList[4] = document.querySelector("#ch5");
+let channelContainer = document.getElementById("channelContainer");
+console.log(channelContainer);
+let OrgchannelElement = document.getElementsByClassName("sidebar__subInfo_card")[0];
+console.log(OrgchannelElement);
 
-let pi = []
-pi[0] = document.querySelector("#chi1");
-pi[1] = document.querySelector("#chi2");
-pi[2] = document.querySelector("#chi3");
-pi[3] = document.querySelector("#chi4");
-pi[4] = document.querySelector("#chi5");
+let ApiKey = "&key=AIzaSyCN1SYd55iUhGcU5s6eR2wgxTVCcjsd6hE";
+let chidApi = "https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet%2CcontentDetails&mine=true";
+let ApiUrl = chidApi + "&pageToken=CAUQAA" + ApiKey;
 
-fetch("https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet%2CcontentDetails&mine=true&key=AIzaSyD9DQ5vZPpX8eWpF6z8-E7c1N2Ts8V4kAA",{
-  headers:{
-    "Authorization":`Bearer ${info['access_token']}`,
-    "Accept": 'application/json'
-  }
-})
-.then((data) => {
-  return data.json();
-}).then((returnedData) => {
-  console.log(returnedData);
-  let idx = 0;
-  for(const element of returnedData.items)
+let nextPageToken = "";
+let res = 0;
+
+async function getall(){
+  for(let k = 0; k<100; k++)
   {
-    let text = element.snippet.title;
-    const textNode = document.createTextNode(text);
-    channelList[idx].appendChild(textNode);
-    const urr = element.snippet.thumbnails.medium.url;
-    console.log(urr);
-    pi[idx].setAttribute('src', urr);
-    pi[idx].addEventListener('click', function(){
-      let newLink = "https://m.youtube.com/channel/" + element.snippet.resourceId.channelId;
-      console.log(newLink);
-      window.location.href = newLink;
+    let response = await fetch(ApiUrl,{
+      headers:{
+        "Authorization":`Bearer ${info['access_token']}`,
+        "Accept": 'application/json'
+      }
     })
-    idx++;
+    let returnedData = await response.json();
+    getthedata(returnedData);
+    console.log(res);
+    if(res + returnedData.pageInfo.resultsPerPage == returnedData.pageInfo.totalResults)
+      k = 101;
   }
-})
+} 
+getall().catch(error => {
+  console.log("error");
+});
+
+OrgchannelElement.remove();
+
+function getthedata(returnedData)
+{
+    if(returnedData.length == 0)
+      isEmpty = true;
+    for(const element of returnedData.items)
+    {
+      res++;
+      let text = element.snippet.title;
+      const textNode = document.createTextNode(text);
+      const urr = element.snippet.thumbnails.medium.url;
+      let channelElement = OrgchannelElement.cloneNode(true);
+      let elementOfChannelElement = channelElement.childNodes;
+      //console.log(elementOfChannelElement);
+      let channelImg = elementOfChannelElement[1].childNodes[1];
+      let channelName= elementOfChannelElement[3];
+      channelImg.setAttribute('src', urr);
+      channelName.appendChild(textNode);
+      //console.log(textNode);
+      channelContainer.appendChild(channelElement);
+      //console.log(channelContainer);
+      channelElement.addEventListener('click', function(){
+      let newLink = "https://www.youtube.com/channel/" + element.snippet.resourceId.channelId;
+      window.location.href = newLink;
+      })
+    }
+    nextPageToken = "&pageToken=" + returnedData.nextPageToken;
+    ApiUrl = chidApi + nextPageToken + ApiKey;
+}
+
+
+// function getData(ApiUrl){
+//   fetch(ApiUrl,{
+//     headers:{
+//       "Authorization":`Bearer ${info['access_token']}`,
+//       "Accept": 'application/json'
+//     }
+//   })
+//   .then((data) => {
+//     return data.json();
+//   }).then((returnedData) => {
+//     console.log(returnedData);
+//     console.log(ApiUrl);
+//     if(returnedData.length == 0)
+//       isEmpty = true;
+//     for(const element of returnedData.items)
+//     {
+//       let text = element.snippet.title;
+//       const textNode = document.createTextNode(text);
+//       const urr = element.snippet.thumbnails.medium.url;
+//       let channelElement = OrgchannelElement.cloneNode(true);
+//       let elementOfChannelElement = channelElement.childNodes;
+//       //console.log(elementOfChannelElement);
+//       let channelImg = elementOfChannelElement[1].childNodes[1];
+//       let channelName= elementOfChannelElement[3];
+//       channelImg.setAttribute('src', urr);
+//       channelName.appendChild(textNode);
+//       //console.log(textNode);
+//       channelContainer.appendChild(channelElement);
+//       console.log(channelContainer);
+//       channelElement.addEventListener('click', function(){
+//       let newLink = "https://www.youtube.com/channel/" + element.snippet.resourceId.channelId;
+//       window.location.href = newLink;
+//       })
+//     }
+//     nextPageToken = "&pageToken=" + returnedData.nextPageToken;
+//     ApiUrl = chidApi + nextPageToken + ApiKey;
+//   })
+// }
+
+
+
 
 
 
